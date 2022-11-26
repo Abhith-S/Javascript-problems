8c0d73c0fe07b90c94afc1446d6cbea2eebe880d
Abhith Shaji <abhithshaji@gmail.com>
2022-11-26 23:42:43 +0530

    Update sha256Hash.js

--- sha256Hash.js
+++ sha256Hash.js
@@ -8,8 +8,6 @@
 
 const { sha256 } = require("ethereum-cryptography/sha256");
 const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
-// should do "npm install ethereum-cryptography" to use above dependencies
-//github repo link - https://github.com/ethereum/js-ethereum-cryptography
 
 // the possible colors that the hash could represent
 const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
@@ -20,28 +18,15 @@ function findColor(hash) {
     //convert utf8 strings to bytes, we use utf8ToBytes()
     //take sha256 hash of these bytes, we use sha256()
     //to compare we need to first convert the hash into string so we use toHex()
-    if(toHex(hash) == toHex(sha256(utf8ToBytes(COLORS[0])))){
-        return "red";
+    //now we loop through the array comparing the strings
+    for(let color of COLORS){
+        if(toHex(hash) == toHex(sha256(utf8ToBytes(color)))){
+         return color;
+        }
+ 
     }
-    else if(toHex(hash) == toHex(sha256(utf8ToBytes(COLORS[1])))){
-        return "green";
-    }
-    else if(toHex(hash) == toHex(sha256(utf8ToBytes(COLORS[2])))){
-        return "blue";
-    }
-    else if(toHex(hash) == toHex(sha256(utf8ToBytes(COLORS[3])))){
-        return "yellow";
-    }
-    else if(toHex(hash) == toHex(sha256(utf8ToBytes(COLORS[4])))){
-        return "pink";
-    }
-    else{
-        return "orange";
-    }
-
-   
-
 }
 
 //test out the code using getting the hash of the color names ,ie, the strings like red,green etc and then passing them to findColor("input hash here");
 
+module.exports = findColor;
