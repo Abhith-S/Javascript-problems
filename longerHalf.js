9bdb1a1d53725b412cd67787f5aab774895200f0
Abhith Shaji <abhithshaji@gmail.com>
2022-11-30 23:37:16 +0530

    Create longerHalf.js

---
+++ longerHalf.js
@@ -0,0 +1,17 @@
+//a string with 'x' will be given . find the longer half of the string before and after the 'x'
+
+function splitAtX(string) {
+  
+  //to get lower half slice from index 0 to index of 'x'
+    let lower = string.slice(0,string.indexOf('x'));
+  
+  //to get upper half slice from index of 'x' to end
+    let upper = string.slice(string.indexOf('x')+1);
+
+    if(lower.length > upper.length)
+    return lower
+    else
+    return upper
+}
+
+module.exports = splitAtX;
