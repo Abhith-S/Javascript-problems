e456e523c8f5f5c1f312ff75b91b60abda832001
Abhith Shaji <abhithshaji@gmail.com>
2022-12-05 23:03:39 +0530

    Create handleUndefined.js

---
+++ handleUndefined.js
@@ -0,0 +1,10 @@
+//The double function here is almost complete! We also want double to be able to handle when x is undefined.
+//If no value is provided for x, return 0.
+//else return double of x
+
+function double(x) {
+    return (x || 0) * 2;
+    
+}
+
+double()
